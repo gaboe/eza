040fe2582d944900ba662ce195ef9e7005b5ac86
gaboe <gabrielecegi@gmail.com>
2018-03-31 09:49:38 +0200

    add dependencies on graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import * as React from 'react';
+import * as React from "react";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
 
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: process.env.REACT_APP_SERVER_URL,
+    credentials: "include"
+  }),
+  cache: new InMemoryCache(),
+  defaultOptions: {
+    query: {
+      fetchPolicy: "network-only"
+    }
+  }
+});
 class App extends React.Component {
   render() {
     return (
       <>
-        <p>Eza</p>
+        <ApolloProvider client={client}>
+          <p>Eza</p>
+        </ApolloProvider>
       </>
     );
   }
