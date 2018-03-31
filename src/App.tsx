b97d0233828c03c522d73187605454245ecf1a9f
gaboe <gabrielecegi@gmail.com>
2018-03-31 16:11:53 +0200

    Fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
-import { DatabaseExplorer } from "./components/database-exploler/DatabaseExpoler";
+import { DatabaseExplorer } from "./components/database-explorer/DatabaseExplorer";
 
 const client = new ApolloClient({
   link: new HttpLink({
